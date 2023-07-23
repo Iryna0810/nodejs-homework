24b27d03d6330e4570dfbfc447e0ba7b988eea3a
Rew <irinavakulenko2000@gmail.com>
2023-07-24 00:08:57 +0300

    start

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-// const app = require('./app')
-
 import app from './app.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+ 
+dotenv.config();
+const { DB_HOST, PORT } = process.env;
+
+
+//TgqkK8$dGVA@WRe
+
+// const DB_HOST = "mongodb+srv://iryna-node-js:TgqkK8$dGVA@WRe@cluster0.nynyolw.mongodb.net/db-contacts?retryWrites=true&w=majority"
+//mongodb+srv://iryna-node-js:<password>@cluster0.nynyolw.mongodb.net/?retryWrites=true&w=majority
+//mongodb+srv://iryna-node-js:TgqkK8$dGVA@WRe@cluster0.nynyolw.mongodb.net/
+mongoose.set('strictQuery', true);
+// console.log(process.env)
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => console.log("Server running. Use our API on port: 3000"))
+  })
+  .catch(error => {
+  console.log(error.message);
+    process.exit(1);
+}
+  )
+
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
