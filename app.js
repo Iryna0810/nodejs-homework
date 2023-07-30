32fbf4076933c1607f408d4c435b144b76596a56
Rew <irinavakulenko2000@gmail.com>
2023-07-30 23:51:26 +0300

    start hw04

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
+import authRouter from './routes/api/auth-routher.js';
 import contactsRouter from './routes/api/contacts.js'
 
 const app = express()
@@ -11,6 +12,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+
+app.use("/api/auth-router", authRouter);
 
 app.use('/api/contacts', contactsRouter)
 
