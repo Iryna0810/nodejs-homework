4f9d254f5493a043b7ce8317b8e7fee3a214cb20
Rew <irinavakulenko2000@gmail.com>
2023-08-04 01:00:04 +0300

    Update authenticate.js

--- middlewars/authenticate.js
+++ middlewars/authenticate.js
@@ -9,32 +9,29 @@ const { JWT_SECRET } = process.env;
 
 const authenticate = async (req, res, next) => {
     try {
-    const { authorization = "" } = req.headers;
-    const [ bearer, token ] = authorization.split(' ');
-    if (bearer !== "Bearer") {
-      throw HttpError(401, error.message)  
+        const { authorization = "" } = req.headers;
+        const [bearer, token] = authorization.split(' ');
+        if (bearer !== "Bearer") {
+            throw HttpError(401, error.message)
         }
         
         try {
             const { id } = jwt.verify(token, JWT_SECRET);
             const user = await User.findById(id);
             if (!user || !user.token) {
-                throw HttpError(401, "Not authorized")  
+                throw HttpError(401, "Not authorized")
             }
             req.user = user;
             next();
         } catch (error) {
-                  throw HttpError(401, "Not authorized")  
+            throw HttpError(401, "Not authorized")
 
         }
 
     } catch (error) {
         next(error);
     }
+};
 
-    //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0Y2JmYWM3ZGFlMWFiYjIwMTVkMDg3MiIsImlhdCI6MTY5MTA4OTYzMiwiZXhwIjoxNjkxMTcyNDMyfQ.euBQVL51xmHdqC92cf6oGMqWo_aMjU92xxBaJDoLyfU
-     
-
-}
 
 export default authenticate;
